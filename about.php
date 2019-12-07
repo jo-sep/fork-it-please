958187a1084658e3b71dba4f16787331a354b5b2
jo-sep <alleluia0892@gmail.com>
2019-12-07 14:31:09 +0800

    updated about.php

--- about.php
+++ about.php
@@ -10,7 +10,73 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container"></div>
+  <div class="container">
+  	<a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link " href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link active" href="about.php">About</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link " href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-12">
+        <h1 class="font-weight-light">ABOUT KAL Skills Akademie</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi voluptate placeat similique hic! Repellendus totam porro non asperiores eius nam repudiandae quibusdam nesciunt aperiam obcaecati. Voluptate molestias veniam iure eos!</p>
+      </div>
+
+      <div class = "col-lg-12">
+        <img src = "./images/61798466_420082855496735_2809800272564453376_n.png" class="img-fluid w-100" alt="Responsive image">
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Well -->
+    <div class = "row">
+      <h3>School Reviews:</h3>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+      
+      <div class = "col-lg-12 col-md-6 col-sm-4">
+        <ul>
+          <li>
+            They are so great, highly recommendable...! learning exp in this school is second to none.
+          </li>
+          <li>
+            Great Facilities, Great Trainors, Great Equipments, Great Teachings.
+          </li>
+          <li>
+            KAL Skills Akademie is a school for COMPETENCE, SKILLS, AND ABILITIES. Highly recommended. 5 stars
+          </li>
+          <li>
+            Amazing school to enhance your skills. I learned a lot from this school they deserve a 5 stars :)
+          </li>
+        </ul>
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
   <!-- /.container -->
 
  <?php include_once("./components/footer.php"); ?>
