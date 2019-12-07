3a2df0ac321eb46f93538d788075efd6a2e58440
James Roncesvalles <roncy.pro13@gmail.com>
2019-12-07 10:30:14 +0800

    added first files

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include_once("./components/header.php"); ?>
+
+<body>
+
+  <?php 
+    include_once("./components/menu.php");
+  ?>
+
+  <!-- Page Content -->
+  <div class="container"></div>
+  <!-- /.container -->
+
+  <?php include_once("./components/footer.php"); ?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include_once("./components/header.php"); ?>
+
+<body>
+
+  <?php 
+    include_once("./components/menu.php");
+  ?>
+
+  <!-- Page Content -->
+  <div class="container"></div>
+  <!-- /.container -->
+
+ <?php include_once("./components/footer.php"); ?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
