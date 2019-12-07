d0ec02f1c36a8c14ae3b6e414528c453f9e91e80
RedMarshall047 <red.marshall.law@gmail.com>
2019-12-07 14:27:57 +0800

    contact-page added

--- contact.php
+++ contact.php
@@ -10,7 +10,47 @@
   ?>
 
   <!-- Page Content -->
-  <div class="container"></div>
+  <div class="container">
+  	
+  	<!-- Heading Row -->
+	    <div class="row align-items-center my-5">
+	      <!-- /.col-lg-8 -->
+	      <div class="col-lg-12">
+	        <h1 class="font-weight-light">Page 3</h1>
+	      </div>
+	      <!-- /.col-md-4 -->
+	    </div>
+	    <!-- /.row -->
+
+	    <div class = "row">
+	      <div class = "col-lg-12 col-md-12 col-sm-12 mb-2">
+	        <h3>Basic Info:</h3>
+	      </div>
+
+	      <div class = "col-lg-12">
+	        <ul>
+	          <li>
+	            Address: 24 Geronimo Street, Sta Monica, Novaliches Quezon City, Philippines
+	          </li>
+	          <li>
+	            Phone: 0966 355 3498
+	          </li>
+	          <li>
+	            Address: <a href = "https://www.facebook.com/kalskills/">https://www.facebook.com/kalskills/</a>
+	          </li>
+	        </ul>
+	      </div>
+
+	      <div class = "col-lg-12">
+	        <h3>Location:</h3>
+	      </div>
+
+	      <div class = "col-lg-12">
+	        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3858.8382183568638!2d121.03989531484157!3d14.721736989725388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b14ed7c27a0d%3A0x629e025b45feec49!2sKal%20Skills%20Akademie%20Inc!5e0!3m2!1sen!2sph!4v1575592050164!5m2!1sen!2sph" width="800" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+	      </div>
+	    </div>
+
+  </div>
   <!-- /.container -->
 
   <?php include_once("./components/footer.php"); ?>
